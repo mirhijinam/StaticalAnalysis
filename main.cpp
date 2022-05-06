946629cd49784d9c13211a8492bfc260cdf1425b
Ibrahim <edinstvenniy123@gmail.com>
2022-05-06 16:32:48 +0300

    fixed code design

--- main.cpp
+++ main.cpp
@@ -8,11 +8,6 @@
 #include <map>
 #include <algorithm>
 
-// Problems:
-// Нет русификации
-// Считывает \n в конце и в начале текста как отдельный символ
-// stopwords_vector() работает только при определении внутри класса
-
 #include <chrono>
 
 int
@@ -167,13 +162,6 @@ WordsCount::stopwords_vector()
 int
 main(int argc, char** argv)
 {
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-
-    auto t1 = high_resolution_clock::now();
-
     std::string pathMainFile = "shakespeare-text.txt";
     WordsCount wordsCount;
     std::string originalText = wordsCount.read_file(pathMainFile);
@@ -183,17 +171,6 @@ main(int argc, char** argv)
     std::cout << cleanText << " - после" << std::endl;
     std::map<std::string, int> count = wordsCount.counter();
     SortPrintMap(count);
-
-    auto t2 = high_resolution_clock::now();
-
-    /* Getting number of milliseconds as an integer. */
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-
-    /* Getting number of milliseconds as a double. */
-    duration<double, std::milli> ms_double = t2 - t1;
-
-    std::cout << ms_int.count() << "ms\n";
-    std::cout << ms_double.count() << "ms\n";
 
     return 0;
 }
