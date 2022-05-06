08d42889e47f13697efe60ec0ff107522c03b865
Ibrahim <edinstvenniy123@gmail.com>
2022-05-06 16:23:03 +0300

    fixed code design

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 // Считывает \n в конце и в начале текста как отдельный символ
 // stopwords_vector() работает только при определении внутри класса
 
+#include <chrono>
+
 int
 InVector(char ch, const std::vector<char>& vec)
 {
@@ -23,7 +25,8 @@ InVector(char ch, const std::vector<char>& vec)
     return 0;
 }
 
-void PrintMap(std::map<std::string, int> const& m)
+void
+SortPrintMap(std::map<std::string, int> const& m)
 {
     std::vector<std::pair<std::string, int>> vec;
     vec.reserve(m.size()); // ????????????????????
@@ -49,38 +52,20 @@ void PrintMap(std::map<std::string, int> const& m)
     out.close();
 }
 
-class
-WordsCount
-{
-private:
-    std::string text;
-    std::string cleanText;
-    std::string stopWords;
-    std::map<std::string, int> mapWordFreq;
+class WordsCount {
+    private:
+        std::string text;
+        std::string cleanText;
+        std::string stopWords;
+        std::map<std::string, int> mapWordFreq;
 
-public:
-    std::string read_file(const std::string& path);
-    std::string erase_separator();
-    std::map<std::string, int> counter();
-    void check_exist(const std::string& word);
+    public:
+        std::string read_file(const std::string& path);
+        std::string erase_separator();
+        std::map<std::string, int> counter();
+        void check_exist(const std::string& word);
 
-    std::vector<std::string>
-    stopwords_vector()
-    {
-        std::string pathStopWordsFile = "stop-words.txt";
-        char delimiter = ',';
-        std::string noNeedWords = read_file(pathStopWordsFile);
-        std::vector<std::string> noNeedWordsVector;
-        size_t pos = 0;
-        std::string token;
-        while((pos = noNeedWords.find(delimiter)) != std::string::npos) {
-            token = noNeedWords.substr(0, pos);
-            noNeedWordsVector.push_back(token);
-            noNeedWords.erase(0, pos + 1); // + 1, т.к. ','
-        }
-
-        return noNeedWordsVector;
-    }
+    std::vector<std::string> stopwords_vector();
     std::vector<char> separators = {'.', '!', '?', ',', ';', ':', '-', '\n', '\t'};
     std::vector<char> sepsToSpaces = {'\n', '\t'};
 
@@ -144,7 +129,8 @@ WordsCount::counter()
     return mapWordFreq;
 }
 
-void WordsCount::check_exist(const std::string& word)
+void
+WordsCount::check_exist(const std::string& word)
 {
     std::vector<std::string> vec = stopwords_vector();
     if (std::find(vec.begin(), vec.end(), word) != vec.end()) {
@@ -160,22 +146,54 @@ void WordsCount::check_exist(const std::string& word)
     }
 }
 
+std::vector<std::string>
+WordsCount::stopwords_vector()
+{
+    std::string pathStopWordsFile = "stop-words.txt";
+    char delimiter = ',';
+    std::string noNeedWords = read_file(pathStopWordsFile);
+    std::vector<std::string> noNeedWordsVector;
+    size_t pos = 0;
+    std::string token;
+    while((pos = noNeedWords.find(delimiter)) != std::string::npos) {
+        token = noNeedWords.substr(0, pos);
+        noNeedWordsVector.push_back(token);
+        noNeedWords.erase(0, pos + 1); // + 1, т.к. ','
+    }
+
+    return noNeedWordsVector;
+}
+
 int
 main(int argc, char** argv)
 {
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+    auto t1 = high_resolution_clock::now();
+
     std::string pathMainFile = "shakespeare-text.txt";
-    //std::string pathStopWordsFile = "stop-words.txt";
     WordsCount wordsCount;
     std::string originalText = wordsCount.read_file(pathMainFile);
     std::cout << originalText << " - до" << std::endl;
-
     std::string cleanText;
     cleanText = wordsCount.erase_separator();
     std::cout << cleanText << " - после" << std::endl;
-
     std::map<std::string, int> count = wordsCount.counter();
+    SortPrintMap(count);
 
-    PrintMap(count);
+    auto t2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
 
     return 0;
 }
